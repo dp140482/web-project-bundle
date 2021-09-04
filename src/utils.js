07221a8b552e9a1615e06c45cef0adadc9f02f06
Dmitry <dp_140482@mail.ru>
2021-09-04 11:38:05 +0300

    Оптимизация

--- src/utils.js
+++ src/utils.js
@@ -3,26 +3,3 @@ export const formatError = text => `
     ${text}
 </span>
 `;
-
-function loadScript(urlOrCallback, callback) {
-    if (typeof urlOrCallback === 'function') {
-        // Значит, передали Callback, и по логике define он составит тело скрипта
-        let element = document.createElement("script");
-        element.innerText = urlOrCallback;
-        document.body.appendChild(element);
-    } else if (typeof urlOrCallback === 'object') {
-        /* Значит, передан массив строк. Полагаем, отсортированный в порядке
-        зависимости и без повторений. */
-        for (let url of urlOrCallback) {
-            let element = document.createElement("script");
-            element.src = url;
-            element.onload = callback;
-            document.body.appendChild(element);
-        }
-    } else {
-        let element = document.createElement("script");
-        element.src = url;
-        element.onload = callback;
-        document.body.appendChild(element);
-    }
-}
